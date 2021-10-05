9eeafa491c5f3b07140d45081728b280423f6a77
Aripov Sergey <aripovsergey3@gmail.com>
2021-10-05 22:28:37 +0500

    web initial commit

---
+++ web1/Models/SiteTypes/InfoSite.cs
@@ -0,0 +1,17 @@
+﻿using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+using System.Collections.Generic;
+
+namespace web1.Models.SiteTypes
+{
+    [SiteType(Title = "Info site")]
+    public class InfoSite : SiteContent<InfoSite>
+    {
+        
+
+        [Region(Title = "Footer", Display = RegionDisplayMode.Setting,ListTitle ="Footer columns")]
+        public IList<HtmlField> FooterContents { get; set; }
+    }
+}
